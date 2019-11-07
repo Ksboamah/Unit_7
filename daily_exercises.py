329654b8b3e6cb9248ca82b285bdb9458bb46726
Kwame <bkwamesakyi@gmail.com>
2019-11-07 11:21:23 -0500

    I started my daily exercises.

---
+++ daily_exercises.py
@@ -0,0 +1,45 @@
+# original = "abcdefghij"
+# first = original[0:2]
+# last = original[3:10]
+# final = first + last
+# print(final)
+
+# for letters in "Pyhton":
+#     print(letters)
+
+
+# def lacking_ends():
+#     word = input("Type in a word consisting of 3 or more letters.")
+#     if len(word) >= 2:
+#         print(word[1:len(word) - 1])
+
+
+# def longest_word(words):
+#     longest = len(words[0])
+#     for x in words:
+#         if len(x) > longest:
+#             longest = len(x)
+#     print(longest)
+
+
+# def upper_lower():
+#     sentence = input("Type in a sentence and watch it transform.")
+#     print(sentence.upper())
+#     print(sentence.lower())
+
+
+def title_case():
+    sentence_2 = input("Type in a Sentence and watch it become a title.")
+    new_sentence_2 = []
+    new_sentence_2.append(sentence_2.split(" "))
+    for x in new_sentence_2:
+
+    sentence_2.upper(sentence_2[len])
+
+
+def main():
+    # lacking_ends()
+    # longest_word(["dog", "fish", "bear", "horse", "falcon", "Phoenix"])
+    # upper_lower()
+
+main()
